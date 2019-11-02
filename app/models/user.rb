16ea536f07674bef6e919ddc359bec2568f6c394
fujimoto <fujimoto@fujimotonoMacBook-Air.local>
2019-11-02 14:00:05 +0900

    rails04 task

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  # nameとemailの入力は必須 nameは15文字まで
+  # nameは入力必須、最大15文字まで
   validates :name, presence: true, length: { maximum: 15 }
+  # emailは入力必須、~@~.~の入力形式
   validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
   # passwordの文字列は、半角英数字1文字以上含む8文字から32文字まで
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i }
