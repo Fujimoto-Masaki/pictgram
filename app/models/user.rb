c3be8cdef801d06f6d4dfd6a924121e24318ff9a
fujimoto <fujimoto@fujimotonoMacBook-Air.local>
2019-11-10 11:35:51 +0900

    commit in

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # emailは入力必須、~@~.~の入力形式
   validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
   # passwordの文字列は、半角英数字1文字以上含む8文字から32文字まで
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i }
+  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i }
 
   has_secure_password
 end
