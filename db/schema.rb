d761388bed6551e034abced9b5fa2cb481ab355b
fujimoto <fujimoto@fujimotonoMacBook-Air.local>
2019-12-30 16:55:01 +0900

    commit in

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_20_105705) do
+ActiveRecord::Schema.define(version: 2019_12_30_071801) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "content"
     t.bigint "topic_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
     t.index ["topic_id"], name: "index_comments_on_topic_id"
   end
 
